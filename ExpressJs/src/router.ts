1945acf919e32a0ff43893b09b4c25b2512eeaf4
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-12-24 12:10:56 +0300

    declared router using express Router

---
+++ ExpressJs/src/router.ts
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+
+const router  = Router();
+
+// All product routes "user stories"
+router.get('/product',() => {})
+router.get('/product/:id',() =>{})
+router.put('/product/:id',() =>{})
+router.delete('/product/:id',() =>{})
+router.post('/product/:id',() =>{})
