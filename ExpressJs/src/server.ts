538c86321cbaf4dcba6646c45be18897b7bac9c9
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-01 12:12:48 +0300

    added a prefix url

--- ExpressJs/src/server.ts
+++ ExpressJs/src/server.ts
@@ -1,12 +1,28 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan'
+
 
 const app = express();
+
+// inbuilt middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }));
+
+// custom middlewares
+app.use((req,res,next)=>{
+    req.shhh_secret = 'doggy'
+    next()
+})
 
 app.get('/',(req, res)=>{ // takes in 2 parameters. route extension and req, res parenthesis
     console.log('herrlo from express')
     res.status(200)
     res.json({message: 'Hello'})
 })
+
+app.use('/api', router)
 
 export default app
 
