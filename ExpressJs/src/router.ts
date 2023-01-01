5fc33326d28e11d3b93fadae3c09c146663d3a55
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-01 12:10:16 +0300

    created a router for user updates

--- ExpressJs/src/router.ts
+++ ExpressJs/src/router.ts
@@ -3,8 +3,30 @@ import {Router} from 'express';
 const router  = Router();
 
 // All product routes "user stories"
-router.get('/product',() => {})
+router.get('/product',(req, res) => {
+    res.json({message: req.shhh_secret})
+})
 router.get('/product/:id',() =>{})
 router.put('/product/:id',() =>{})
 router.delete('/product/:id',() =>{})
 router.post('/product/:id',() =>{})
+
+
+// all update user stories
+
+router.get('/update',() => {})
+router.get('/update/:id',() =>{})
+router.put('/update/:id',() =>{})
+router.delete('/update/:id',() =>{})
+router.post('/update/:id',() =>{})
+
+
+// all updatePoint user stories
+
+router.get('/updatepoint',() => {})
+router.get('/updatepoint/:id',() =>{})
+router.put('/updatepoint/:id',() =>{})
+router.delete('/updatepoint/:id',() =>{})
+router.post('/updatepoint/:id',() =>{})
+
+export default router
