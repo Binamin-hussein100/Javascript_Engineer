63d5f52cc8fdf1850355ddaa38bd1be349213391
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-02 10:18:28 +0300

    routes protected by validating jwt tokens

--- ExpressJs/src/modules/auth.ts
+++ ExpressJs/src/modules/auth.ts
@@ -5,9 +5,27 @@ export const createJWT = (user)=>{
       return token
 }
 
-export const protect = (req,res) =>{
+export const protect = (req,res, next) =>{
     const bearer = req.headers.authorization
     if(!bearer){
+        res.status(401)
+        res.json({message: 'not authorized'})
+        return
+    }
+
+    const [,token] = bearer.split(' ')
+
+    if(!token){
+        res.status(401)
+        res.json({message: 'not authorized'})
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+    } catch (error) {
         res.status(401)
         res.json({message: 'not authorized'})
         return
