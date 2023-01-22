78b83ac4f4779f875f41ba950d0d843418af7ccc
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-22 10:29:22 +0300

    initial configuration

--- MongooseExpress/app.js
+++ MongooseExpress/app.js
@@ -5,4 +5,22 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
+app.set('port', (process.env.PORT || 8081))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors())
+app.use('/api',api)
+app.use(express.static('static'))
+app.use(morgan('dev'))
+
+app.use(function(){
+    const err = new Error("Not Found!")
+    err.status = 404
+    resizeBy.json(err)
+})
+
+app.listen(app.get('port'), function(){
+    console.log('listening on port')
+});
