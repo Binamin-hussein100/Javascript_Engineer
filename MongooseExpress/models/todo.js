bab32d7cdb1e5a1f59996519ac988840e122cb89
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-22 12:36:30 +0300

    declared a todo model

---
+++ MongooseExpress/models/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+const TodoSchema = new mongoose.Schema({
+    record:{type:String, required:true},
+    date:{type:Number,default:100},
+
+})
+
+const model = mongoose.model('TodoModel', TodoSchema)
+
+module.exports = model;
